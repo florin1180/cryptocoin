b90f88d92f3274c1ce06eea7164fcc5a180b0198
rnflorin <florin.1180@gmail.com>
2021-07-23 05:48:32 +0300

    23.07.2021

--- screens/CryptoDetail.js
+++ screens/CryptoDetail.js
@@ -16,139 +16,240 @@ import { VictoryScatter, VictoryLine, VictoryChart, VictoryAxis } from "victory-
 
 import { VictoryCustomTheme } from '../styles'
 
-import { HeaderBar, CurrencyLabel } from '../components';
+import { HeaderBar, CurrencyLabel, TextButton } from '../components';
 
 import { dummyData, COLORS, SIZES, FONTS, icons } from '../constants';
 
 const CryptoDetail = ({ route, navigation }) => {
     
+    const scrollX = new Animated.Value(0)
+    const numberOfCharts = [1, 2, 3]
+
+
     const [selectedCurrency, setSelectedCurrency] = React.useState(null)
+
+    const [chartOptions, setChartOptions] = React.useState(dummyData.chartOptions)
+    const[selectedOptions, setSelectedOptions] = React.useState(chartOptions[0])
+
 
     React.useEffect(() => {
         const {currency} = route.params
         setSelectedCurrency(currency)
     }, [])
 
-    return (
-        <View 
-            style={{
-                marginTop:SIZES.padding,
-                marginHorizontal:SIZES.radius,
-                alignItems:'center',
-                borderRadius:SIZES.radius,
-                backgroundColor: COLORS.white,
-                ...styles.shadow
-            }}
-        >
-            <View
+    function optionOnClickHandler(option) {
+        setSelectedOptions(option)
+    }
+
+
+    function renderDots() {
+        const dotPosition = Animated.divide(scrollX, SIZES.width)
+
+        return (
+            <View style={{height:30, marginTop:15}}>
+                <View 
+                    style={{
+                        flexDirection:'row',
+                        alignItems:'center',
+                        justifyContent:'center'
+                    }}
+                >
+                    {numberOfCharts.map((item, index) => {
+                        const opacity = dotPosition.interpolate({
+                            inputRange: [index - 1, index, index + 1],
+                            outputRange: [0.3, 1, 0.3],
+                            extrapolate:'clamp'
+                        })
+                    })}
+
+                </View>
+
+            </View>
+        )
+    }
+
+    function renderChart() {
+        return (
+            <View 
                 style={{
-                    flexDirection:'row',
                     marginTop:SIZES.padding,
-                    paddingHorizontal:SIZES.padding
+                    marginHorizontal:SIZES.radius,
+                    alignItems:'center',
+                    borderRadius:SIZES.radius,
+                    backgroundColor: COLORS.white,
+                    ...styles.shadow
                 }}
             >
-                <View 
+                <View
                     style={{
                         flexDirection:'row',
                         marginTop:SIZES.padding,
                         paddingHorizontal:SIZES.padding
                     }}
                 >
-                    <View style={{flex:1}}>
-                        <CurrencyLabel
-                            icon={selectedCurrency?.image}
-                            currency={selectedCurrency?.currency}
-                            code={selectedCurrency?.code}
-                        />
+                        <View style={{flex:1}}>
+                            <CurrencyLabel
+                                icon={selectedCurrency?.image}
+                                currency={selectedCurrency?.currency}
+                                code={selectedCurrency?.code}
+                            />
+                        </View>
+
+                        <View>
+                            <Text style={{...FONTS.h3}}>${selectedCurrency?.amount}</Text>
+                            <Text style={{color:selectedCurrency?.type == "I" ? COLORS.green : COLORS.red, ...FONTS.body3}}>{selectedCurrency?.changes}</Text>
+                        </View>
+
                     </View>
 
-                    <View>
-                        <Text style={{...FONTS.h3}}>${selectedCurrency?.amount}</Text>
-                        <Text style={{color:selectedCurrency?.type == "I" ? COLORS.green : COLORS.red, ...FONTS.body3}}>{selectedCurrency?.changes}</Text>
-                    </View>
-
-                </View>
-
-                <View
-                    style={{
-                        marginTop: -25
-                    }}
-                >
-                    <VictoryChart
-                        theme={VictoryCustomTheme}
-                        height={220}
-                        width={SIZES.width - 40}
+                    <Animated.ScrollView
+                        horizontal
+                        pagingEnabled
+                        scrollEventThrottle={16}
+                        snapToAlignment="center"
+                        showsHorizontalScrollIndicator={false}
+                        decelerationRate={0}
+                        onScroll={Animated.event([
+                            {nativeEvent:{contentOffset:{x:scrollX
+                            }}}
+                        ], {useNativeDriver:false})}
                     >
-                        <VictoryLine
-                            style={{
-                                data:{
-                                    stroke:COLORS.secondary
-                                },
-                                parent:{
-                                    border:"1px solid #ccc"
-                                }
-                            }}
-                            data={selectedCurrency?.charData}
-                            categories={{
-                                x: ["15 MIN", "30 MIN", "45 MIN", "60 MIN"],
-                                y: ["15", "30", "45"]
-                            }}
-                        />
-                        <VictoryScatter
-                            data={selectedCurrency?.charData}
-                            size={7}
-                            style={{
-                                data:{
-                                    fill:COLORS.secondary
-                                }
-                            }}
-                        />
+                        {
+                                numberOfCharts.map((item, idex) => (
+                                    <View
+                                        key={`chart-${index}`}
+                                        style={{
+                                            marginLeft: index == 0 ? SIZES.base: 0
+                                        }}
+                                    >
+                    <View
+                        style={{
+                            marginTop: -25
+                        }}
+                    >
+                        <VictoryChart
+                            theme={VictoryCustomTheme}
+                            height={220}
+                            width={SIZES.width - 40}
+                        >
+                            <VictoryLine
+                                style={{
+                                    data:{
+                                        stroke:COLORS.secondary
+                                    },
+                                    parent:{
+                                        border:"1px solid #ccc"
+                                    }
+                                }}
+                                data={selectedCurrency?.charData}
+                                categories={{
+                                    x: ["15 MIN", "30 MIN", "45 MIN", "60 MIN"],
+                                    y: ["15", "30", "45"]
+                                }}
+                            />
+                            <VictoryScatter
+                                data={selectedCurrency?.charData}
+                                size={7}
+                                style={{
+                                    data:{
+                                        fill:COLORS.secondary
+                                    }
+                                }}
+                            />
+                            <VictoryAxis
+                                style={{
+                                    grid:{
+                                        stroke:"transparent"
+                                    }
+                                }}
+                            />
+                            <VictoryAxis
+                                dependentAxis
+                                style={{
+                                    axis:{
+                                        stroke:"transparent"
+                                    },
+                                    grid:{
+                                        stroke:"grey"
+                                    }
+                                }}
+                            />
 
-                    </VictoryChart>
+                        </VictoryChart>
+                    </View>
+                    </View>
+                                ))
+                    }
+                    </Animated.ScrollView>
+
+                    <View
+                        style={{width:'100%', paddingHorizontal:SIZES.padding, flexDirection:'row', justifyContent:'space-between'}}
+                    >
+                        {
+
+                            chartOptions.map((option) => {
+                                return (
+                                    <TextButton
+                                        key={`option-${option.id}`}
+                                        label={option.label}
+                                        customContainerStyle={{
+                                            height:30,
+                                            width:60,
+                                            borderRadius:15,
+                                            backgroundColor: selectedOptions.id == option.id ? COLORS.primary : COLORS.lightGray
+                                        }}
+                                        customLabelStyle={{
+                                            color: selectedOptions.id == option.id ? COLORS.white : COLORS.gray, ...FONTS.body5
+                                        }}
+                                        onPress={() =>
+                                        optionOnClickHandler(option)}
+                                    />
+                                )
+                            })
+                        }
+                    </View>
+                    {renderDots()}
                 </View>
-
-            </View>
-
-        </View>
-    )
+            )
+        }
 
 
-    return (
-        <SafeAreaView
-            style={{
-                flex:1,
-                backgroundColor:COLORS.lightGray1
-            }}
-        >
-            <HeaderBar right={true}/>
+        return (
+            <SafeAreaView
+                style={{
+                    flex:1,
+                    backgroundColor:COLORS.lightGray1
+                }}
+            >
+                <HeaderBar right={true}/>
 
-            <ScrollView>
-                <View style={{flex:1, paddingBottom:SIZES.padding}}>
-                    {renderChart()}
-                </View>
-            </ScrollView>
+                <ScrollView>
+                    <View style={{flex:1, paddingBottom:SIZES.padding}}>
+                        {renderChart()}
+                    </View>
+                </ScrollView>
 
-        </SafeAreaView>
-    )
-}
+            </SafeAreaView>
+        )
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    shadow: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
         },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
+        shadow: {
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 4,
+            },
+            shadowOpacity: 0.30,
+            shadowRadius: 4.65,
 
-        elevation: 8,
-    }
-})
+            elevation: 8,
+        }
+    })
 
-export default CryptoDetail;
+    export default CryptoDetail;
